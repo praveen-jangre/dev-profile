77524ac589fa2bea51ca60fb402906de3e4a0d75
Praveen Bharat Jangre <praveen.jangre@impelsys.com>
2023-06-10 19:19:49 +0530

    first commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Praveen Jangre on 10/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dev-profile/dev-profileUITests/dev_profileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  dev_profileUITestsLaunchTests.swift
+//  dev-profileUITests
+//
+//  Created by Praveen Jangre on 10/06/2023.
+//
+
+import XCTest
+
+final class dev_profileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
